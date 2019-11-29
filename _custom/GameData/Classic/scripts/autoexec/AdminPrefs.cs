adc27f38672b0c13d8eb74c0dd1ffe1d613f0f9b
Anthony Mineo <anthonymineo@gmail.com>
2019-11-29 16:38:53 -0500

    init prefs expander

---
+++ _custom/GameData/Classic/scripts/autoexec/AdminPrefs.cs
@@ -0,0 +1,10 @@
+// These values should be written on container start
+// ---- 
+//$Host::AdminList = "";
+//$Host::AdminPassword = "changeme";
+//$Host::ClassicSuperAdminPassword = "changme";
+//$Host::ClassicTelnetListenPass = "changeme";
+//$Host::ClassicTelnetPassword = "changeme";
+//$Host::SuperAdminList = "";
+// ----
+
